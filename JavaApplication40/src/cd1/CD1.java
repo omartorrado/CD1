4f8867ed90d14154facd3087f483eba7d8964f6b
otorradomiguez <otorradomiguez@victoria2.danielcastelao.org>
2016-10-24 09:39:02 +0200

    numeros random

--- JavaApplication40/src/cd1/CD1.java
+++ JavaApplication40/src/cd1/CD1.java
@@ -5,6 +5,8 @@
  */
 package cd1;
 
+import static java.lang.Math.random;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -20,14 +22,17 @@ public class CD1 {
     public static void main(String[] args) {
         //comentario1
         Scanner sc1=new Scanner(System.in);
-        System.out.println("Escribe un numero menor que 2\no no");
+        System.out.println("numeros aleatorios");
         //experimento
-        int i=sc1.nextInt();
-        if (i>2){
-        System.out.println("Pruebas GIT");
-        }
-        else{
-        System.out.println("Hola mundo");
+
+        Random e=new Random(10);
+        for (int n=0; n<10;n++){
+            if (e.nextInt()>=5){
+            System.out.println("Pruebas GIT");
+            }
+            else{
+            System.out.println("Hola mundo");
+            }
         }
         //Conflicto
        //Comentario
